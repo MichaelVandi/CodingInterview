4cdc17a523384f668f3363bb7ca42b825c645b41
MichaelVandi <mikevee2013@gmail.com>
2020-09-21 13:27:00 -0400

    Added symmetric tree - Easy

---
+++ Easy/09_21_2020_symmetric_tree.java
@@ -0,0 +1,21 @@
+/**
+ * Time: O(n)
+ * Space: O(n)
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
