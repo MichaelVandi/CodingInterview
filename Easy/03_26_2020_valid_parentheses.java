c0dafc81ebda75083c49d49b11bb6281e2db0436
MichaelVandi <mikevee2013@gmail.com>
2021-05-19 16:44:11 -0400

    Added Prompt for Valid Parenthesis - Easy

--- Easy/03_26_2020_valid_parentheses.java
+++ Easy/03_26_2020_valid_parentheses.java
@@ -1,3 +1,10 @@
+/**
+ * Prompt: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    An input string is valid if:
+    1. Open brackets must be closed by the same type of brackets.
+    2. Open brackets must be closed in the correct order.
+ */
+
 class Solution {
     public boolean isValid(String s) {
         
