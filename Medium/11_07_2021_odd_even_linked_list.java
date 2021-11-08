ffb9c8c2f5829c382cea00d3e25a3cf1a9f23082
MichaelVandi <mikevee2013@gmail.com>
2021-11-07 19:55:24 -0800

    Add odd even linked list - Medium

---
+++ Medium/11_07_2021_odd_even_linked_list.java
@@ -0,0 +1,30 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Description: Given the head of a singly linked list, group all the nodes with odd indices together followed by the 
+ * nodes with even indices, and return the reordered list. The first node is considered odd, and the 
+ * second node is even, and so on. Note that the relative order inside both the even and odd groups 
+ * should remain as it was in the input. You must solve the problem in O(1) extra space complexity 
+ * and O(n) time complexity.
+ */
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (odd.next != null && even.next != null) {
+            // Do stuff.
+            odd.next = even.next;
+            odd = odd.next;
+            
+            // Do the same thing fo reven
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
